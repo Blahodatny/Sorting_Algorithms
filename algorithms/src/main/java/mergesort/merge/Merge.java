87163c2bb9c958334858f6a216f2f1e6e46a217b
Dmitriy-Blahodatnyi <dbrlmola82@gmail.com>
2019-02-06 15:58:37 +0200

    fixed problem

--- algorithms/src/main/java/mergesort/merge/Merge.java
+++ algorithms/src/main/java/mergesort/merge/Merge.java
@@ -4,7 +4,7 @@ import java.util.stream.IntStream;
 
 class Merge {
     static void merge(int[] arr, int l, int m, int r) {
-        var left = IntStream.range(0, m - l + 1).map(i -> arr[l + 1]).toArray();
+        var left = IntStream.rangeClosed(0, m - l).map(i -> arr[l + i]).toArray();
         var right = IntStream.range(0, r - m).map(i -> arr[m + i + 1]).toArray();
 
         for (int k = l, i = 0, j = 0; k <= r; k++)
